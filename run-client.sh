f814a7d989e44e854408644184c41fc99d3bfb5a
Anthony Baxter <arb@google.com>
2009-07-22 10:45:55 -0700

    Fixed paths in run-client.sh. Added a .hgignore.

--- run-client.sh
+++ run-client.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-export CLASSPATH="build/core:third_party/jline-0.9.94.jar:third_party/google-collect-1.0-rc2.jar:third_party/protobuf-java-2.1.0.jar"
+export CLASSPATH="build/core:third_party/runtime/jline/jline-0.9.94.jar:third_party/runtime/google_collect/google-collect-1.0-rc2.jar:third_party/protobuf-java-2.1.0.jar"
 java org.waveprotocol.wave.examples.fedone.waveclient.console.ConsoleClient $@
