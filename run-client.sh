3edfb74ae68cdc5181c6ff40141343990b03225a
anthonybaxter@gmail.com <anthonybaxter@gmail.com>
2009-07-24 15:17:44 -0700

    update run-client and run-server to use the jars.

--- run-client.sh
+++ run-client.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-export CLASSPATH="build/core:third_party/runtime/jline/jline-0.9.94.jar:third_party/runtime/google_collect/google-collect-1.0-rc2.jar:third_party/runtime/protobuf/protobuf-java-2.1.0.jar"
-java org.waveprotocol.wave.examples.fedone.waveclient.console.ConsoleClient $@
+java -jar dist/fedone-client-0.2.jar $@
--- run-server.sh
+++ run-server.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-export CLASSPATH="build/core:third_party/jline-0.9.94.jar:third_party/google-collect-1.0-rc2.jar:third_party/protobuf-java-2.1.0.jar:third_party/guice-2.0.jar:third_party/aopalliance.jar"
-java org.waveprotocol.wave.examples.fedone.ServerMain localhost:12345
+java -jar dist/fedone-0.2.jar $*
