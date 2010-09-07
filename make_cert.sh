ba1b8634484e9125e29c2132b9ed476b66602031
Joseph Gentle <josephg@gmail.com>
2010-09-07 12:30:32 +1000

    Cleaned up script a touch

--- make_cert.sh
+++ make_cert.sh
@@ -1,11 +1,24 @@
 #!/bin/bash                                                                       
 
+# This script will make a set of certificates for federation.
+# To actually federate, the certificiates will need to be signed.
+#
+# For instructions, see this wiki page:
+# http://code.google.com/p/wave-protocol/wiki/Certificates
+
 NAME=$1
 
 if [ "$NAME" == '' ]
 then
-  echo "$0 <certificate name>" 1>&2
+  echo "$0 <domain name>" 1>&2
   exit 1
 fi
-openssl genrsa 2048 | openssl pkcs8 -topk8 -nocrypt -out $NAME.key
-openssl req -new -x509 -nodes -sha1 -days 365 -key $NAME.key -out $NAME.crt
+
+echo "1) Generating key for $NAME in '$NAME.key' ..."
+echo
+openssl genrsa 2048 | openssl pkcs8 -topk8 -nocrypt -out "$NAME.key"
+
+echo
+echo "2) Generating certificate request for $NAME in '$NAME.crt' ..."
+echo
+openssl req -new -x509 -nodes -sha1 -days 365 -key "$NAME.key" -out "$NAME.crt"
