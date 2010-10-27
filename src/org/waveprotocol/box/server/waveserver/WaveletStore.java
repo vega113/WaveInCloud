bf7e43f71289dbe927535a940e2f40796d308e0a
soren <devnull@localhost>
2010-10-27 19:55:11 +1100

    WaveletStore interface and DeltaStore-based implementation (not hooked up, no unittests yet)

---
+++ src/org/waveprotocol/box/server/waveserver/WaveletStore.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.waveprotocol.box.server.waveserver;
+
+import org.waveprotocol.wave.model.id.WaveId;
+import org.waveprotocol.wave.model.id.WaveletId;
+import org.waveprotocol.wave.model.id.WaveletName;
+import org.waveprotocol.wave.model.wave.data.ReadableWaveletData;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * Stores wavelets.
+ *
+ * <p>
+ * Callers must serialize all calls to {@link #open(WaveletName)} and
+ * {@link #delete(WaveletName)} on the same wavelet.
+ *
+ * @author soren@google.com (Soren Lassen)
+ */
+public interface WaveletStore {
+
+  /**
+   * Accesses the state for a wavelet.
+   * Permits reading a snapshot of the wavelet state and historical deltas,
+   * and appending deltas to the history.
+   */
+  interface WaveletAccess extends WaveletDeltaRecordReader, Closeable {
+
+    /**
+     * @return Immutable copy of the last known committed state of the wavelet.
+     */
+    ReadableWaveletData getSnapshot();
+
+    /**
+     * Blocking call to append deltas to the end of the delta history.
+     * If the call returns normally (doesn't throw an exception), then
+     * the deltas have been successfully and "durably" stored, that is,
+     * the method forces the data to disk.
+     *
+     * @param deltas Contiguous deltas, beginning from the DeltaAccess
+     *        object's end version. It is the caller's responsibility to
+     *        ensure that everything matches up (applied and transformed
+     *        deltas in the records match, that the hashes are correctly
+     *        computed, etc).
+     * @param resultingSnapshot A snapshot of the state of the wavelet after
+     *        {@code deltas} are applied.
+     * @throws IOException if anything goes wrong with the underlying storage.
+     */
+    void appendDeltas(Collection<WaveletDeltaRecord> deltas,
+        ReadableWaveletData resultingSnapshot) throws IOException;
+  }
+
+  /**
+   * Opens a wavelet, which can be used to store deltas. If the wavelet doesn't
+   * exist, it is implicitly created when the first op is appended to it.
+   *
+   * @throws IOException if anything goes wrong with the underlying storage.
+   */
+  WaveletAccess open(WaveletName waveletName) throws IOException;
+
+  /**
+   * Deletes a non-empty wavelet.
+   *
+   * @throws IOException if anything goes wrong with the underlying storage.
+   * @throws FileNotFoundException if this wavelet doesn't exist in the wavelet
+   *         store.
+   */
+  void delete(WaveletName waveletName) throws FileNotFoundException, IOException;
+
+  /**
+   * Looks up all wavelets with deltas in the wavelet store.
+   *
+   * @throws IOException if anything goes wrong with the underlying storage.
+   */
+  Set<WaveletId> lookup(WaveId waveId) throws IOException;
+}
