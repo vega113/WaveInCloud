a715bfb6e9ea15652d52191c9faa8a10c0e6bcba
David Hearnden <hearnden@google.com>
2010-11-02 18:44:50 +1100

    Adds error indicator.  Also has a bunch of other things squished in, after repairing the repository that hg pwned (why do they make it so much harder than git?)

--- src/org/waveprotocol/box/webclient/client/WebClient.java
+++ src/org/waveprotocol/box/webclient/client/WebClient.java
@@ -25,6 +25,8 @@ import com.google.gwt.dom.client.Element;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -44,7 +46,13 @@ import org.waveprotocol.box.webclient.waveclient.common.ClientIdGenerator;
 import org.waveprotocol.box.webclient.waveclient.common.WaveViewServiceImpl;
 import org.waveprotocol.box.webclient.waveclient.common.WebClientBackend;
 import org.waveprotocol.box.webclient.waveclient.common.WebClientUtils;
+import org.waveprotocol.box.webclient.widget.error.ErrorIndicatorPresenter;
+import org.waveprotocol.wave.client.common.safehtml.SafeHtml;
+import org.waveprotocol.wave.client.common.safehtml.SafeHtmlBuilder;
+import org.waveprotocol.wave.client.common.util.AsyncHolder.Accessor;
 import org.waveprotocol.wave.client.debug.logger.LogLevel;
+import org.waveprotocol.wave.client.scheduler.Scheduler.Task;
+import org.waveprotocol.wave.client.scheduler.SchedulerInstance;
 import org.waveprotocol.wave.client.util.ClientFlags;
 import org.waveprotocol.wave.client.widget.common.ImplPanel;
 import org.waveprotocol.wave.concurrencycontrol.channel.WaveViewService;
@@ -107,16 +115,8 @@ public class WebClient implements EntryPoint {
    * This is the entry point method.
    */
   public void onModuleLoad() {
-    final UncaughtExceptionHandler parent = GWT.getUncaughtExceptionHandler();
-    GWT.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
-      @Override
-      public void onUncaughtException(Throwable e) {
-        LOG.severe("Unhandled exception", e);
-        if (parent != null) {
-          parent.onUncaughtException(e);
-        }
-      }
-    });
+
+    ErrorHandler.install();
 
     // Set up UI
     RootPanel.get("app").add(BINDER.createAndBindUi(this));
@@ -293,5 +293,80 @@ public class WebClient implements EntryPoint {
         contentPanel, id, channel, idGenerator, isNewWave);
     wave.load(null);
     History.newItem(id.serialise(), false);
+  }
+
+  static class ErrorHandler implements UncaughtExceptionHandler {
+    private final UncaughtExceptionHandler next;
+
+    private ErrorHandler(UncaughtExceptionHandler next) {
+      this.next = next;
+    }
+
+    public static void install() {
+      GWT.setUncaughtExceptionHandler(new ErrorHandler(GWT.getUncaughtExceptionHandler()));
+      scheduleException();
+    }
+
+    @Override
+    public void onUncaughtException(Throwable e) {
+      final ErrorIndicatorPresenter error = ErrorIndicatorPresenter.create(RootPanel.get("banner"));
+      getStackTraceAsync(e, new Accessor<SafeHtml>() {
+        @Override
+        public void use(SafeHtml stack) {
+          error.addDetail(stack, null);
+        }
+      });
+
+      if (next != null) {
+        next.onUncaughtException(e);
+      }
+    }
+
+    private void getStackTraceAsync(final Throwable t, final Accessor<SafeHtml> whenReady) {
+      // TODO: Request stack-trace de-obfuscation. For now, just use the
+      // javascript stack trace.
+      //
+      // Use minimal services here, in order to avoid the chance that reporting
+      // the error produces more errors. In particular, do not use Scheduler.
+      // Also, this code could potentially be put behind a runAsync boundary, to
+      // save whatever dependencies it uses from the initial download.
+      DeferredCommand.addCommand(new Command() {
+        @Override
+        public void execute() {
+          SafeHtmlBuilder stack = new SafeHtmlBuilder();
+
+          Throwable error = t;
+          while (error != null) {
+            stack.appendEscaped(error.getMessage()).appendHtmlConstant("<br>");
+            for (StackTraceElement elt : error.getStackTrace()) {
+              stack //
+                  .appendHtmlConstant("  ") //
+                  .appendEscaped(elt.getClassName()).appendHtmlConstant(".") //
+                  .appendEscaped(elt.getMethodName()).appendHtmlConstant(" (") //
+                  .appendEscaped(elt.getFileName()).appendHtmlConstant(":") //
+                  .append(elt.getLineNumber()).appendHtmlConstant(")") //
+                  .appendHtmlConstant("<br>");
+            }
+            error = error.getCause();
+            if (error != null) {
+              stack.appendHtmlConstant("Caused by: ");
+            }
+          }
+
+          whenReady.use(stack.toSafeHtml());
+        }
+      });
+    }
+
+    static void scheduleException() {
+      SchedulerInstance.getLowPriorityTimer().scheduleDelayed(new Task() {
+
+        @Override
+        public void execute() {
+          throw new RuntimeException("test");
+        }
+
+      }, 1000);
+    }
   }
 }
