b355eaa3751dfdfc5da639e25a33f42903b70917
Yuri Z <vega113@gmail.com>
2011-01-31 09:46:43 +1100

    Fix WaveService.fetchWavelet to always return waveletData for existing wave in Wiab

--- src/org/waveprotocol/box/server/robots/active/ActiveApiServlet.java
+++ src/org/waveprotocol/box/server/robots/active/ActiveApiServlet.java
@@ -17,21 +17,13 @@
 
 package org.waveprotocol.box.server.robots.active;
 
-import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
-import com.google.wave.api.InvalidRequestException;
-import com.google.wave.api.JsonRpcResponse;
-import com.google.wave.api.OperationRequest;
-import com.google.wave.api.ProtocolVersion;
 import com.google.wave.api.RobotSerializer;
 import com.google.wave.api.data.converter.EventDataConverterManager;
-import com.google.wave.api.impl.GsonFactory;
-
 import net.oauth.OAuth;
 import net.oauth.OAuthAccessor;
 import net.oauth.OAuthConsumer;
-import net.oauth.OAuthException;
 import net.oauth.OAuthMessage;
 import net.oauth.OAuthServiceProvider;
 import net.oauth.OAuthValidator;
@@ -40,24 +32,15 @@ import net.oauth.server.HttpRequestMessage;
 import org.waveprotocol.box.server.account.AccountData;
 import org.waveprotocol.box.server.persistence.AccountStore;
 import org.waveprotocol.box.server.persistence.PersistenceException;
-import org.waveprotocol.box.server.robots.OperationContext;
-import org.waveprotocol.box.server.robots.OperationContextImpl;
-import org.waveprotocol.box.server.robots.OperationResults;
 import org.waveprotocol.box.server.robots.OperationServiceRegistry;
+import org.waveprotocol.box.server.robots.dataapi.BaseApiServlet;
 import org.waveprotocol.box.server.robots.util.ConversationUtil;
-import org.waveprotocol.box.server.robots.util.LoggingRequestListener;
-import org.waveprotocol.box.server.robots.util.OperationUtil;
 import org.waveprotocol.box.server.waveserver.WaveletProvider;
 import org.waveprotocol.wave.model.wave.InvalidParticipantAddress;
 import org.waveprotocol.wave.model.wave.ParticipantId;
 import org.waveprotocol.wave.util.logging.Log;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.URISyntaxException;
-import java.util.List;
-
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -67,34 +50,20 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author ljvderijk@google.com (Lennard de Rijk)
  */
-public class ActiveApiServlet extends HttpServlet {
+public class ActiveApiServlet extends BaseApiServlet {
 
   private static final Log LOG = Log.get(ActiveApiServlet.class);
-  private static final WaveletProvider.SubmitRequestListener LOGGING_REQUEST_LISTENER =
-      new LoggingRequestListener(LOG);
-  private static final String JSON_CONTENT_TYPE = "application/json";
-
-  private final RobotSerializer robotSerializer;
-  private final EventDataConverterManager converterManager;
-  private final WaveletProvider waveletProvider;
-  private final OperationServiceRegistry operationRegistry;
-  private final ConversationUtil conversationUtil;
-  private final OAuthValidator validator;
   private final OAuthServiceProvider oauthServiceProvider;
   private final AccountStore accountStore;
-
+  
   @Inject
   public ActiveApiServlet(RobotSerializer robotSerializer,
       EventDataConverterManager converterManager, WaveletProvider waveletProvider,
       @Named("ActiveApiRegistry") OperationServiceRegistry operationRegistry,
       ConversationUtil conversationUtil, OAuthServiceProvider oAuthServiceProvider,
       OAuthValidator validator, AccountStore accountStore) {
-    this.robotSerializer = robotSerializer;
-    this.converterManager = converterManager;
-    this.waveletProvider = waveletProvider;
-    this.conversationUtil = conversationUtil;
-    this.operationRegistry = operationRegistry;
-    this.validator = validator;
+    super(robotSerializer, converterManager, waveletProvider, operationRegistry, conversationUtil,
+        validator);
     this.oauthServiceProvider = oAuthServiceProvider;
     this.accountStore = accountStore;
   }
@@ -138,92 +107,6 @@ public class ActiveApiServlet extends HttpServlet {
             oauthServiceProvider);
     OAuthAccessor accessor = new OAuthAccessor(consumer);
 
-    try {
-      validator.validateMessage(message, accessor);
-    } catch (OAuthException e) {
-      LOG.info("The message does not conform to OAuth", e);
-      resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-      return;
-    } catch (URISyntaxException e) {
-      LOG.info("The message URL is invalid", e);
-      resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-      return;
-    }
-
-    String apiRequest;
-    try {
-      // message.readBodyAsString() doesn't work due to a NPE in the OAuth
-      // libraries.
-      BufferedReader reader = req.getReader();
-      apiRequest = reader.readLine();
-    } catch (IOException e) {
-      LOG.warning("Unable to read the incoming request", e);
-      throw e;
-    }
-
-    LOG.info("Received the following Json: " + apiRequest);
-    List<OperationRequest> operations;
-    try {
-      operations = robotSerializer.deserializeOperations(apiRequest);
-    } catch (InvalidRequestException e) {
-      LOG.info("Unable to parse Json to list of OperationRequests: " + apiRequest);
-      resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
-          "Unable to parse Json to list of OperationRequests: " + apiRequest);
-      return;
-    }
-
-    // Create an unbound context.
-    ProtocolVersion version = OperationUtil.getProtocolVersion(operations);
-    OperationContextImpl context = new OperationContextImpl(
-        waveletProvider, converterManager.getEventDataConverter(version), conversationUtil);
-
-    executeOperations(context, operations, participant);
-    handleResults(context, resp, version);
-  }
-
-  /**
-   * Executes operations in the given context.
-   *
-   * @param context the context to perform the operations in.
-   * @param operations the operations to perform.
-   * @param author the author for which to perform the robot operations.
-   */
-  private void executeOperations(
-      OperationContext context, List<OperationRequest> operations, ParticipantId author) {
-    for (OperationRequest operation : operations) {
-      OperationUtil.executeOperation(operation, operationRegistry, context, author);
-    }
-  }
-
-  /**
-   * Handles an {@link OperationResults} by submitting the deltas that are
-   * generated and writing a response to the robot.
-   *
-   * @param results the results of the operations performed.
-   * @param resp the servlet to write the response in.
-   * @param version the version of the protocol to use for writing a response.
-   * @throws IOException if the response can not be written.
-   */
-  private void handleResults(
-      OperationResults results, HttpServletResponse resp, ProtocolVersion version)
-      throws IOException {
-    OperationUtil.submitDeltas(results, waveletProvider, LOGGING_REQUEST_LISTENER);
-
-    List<JsonRpcResponse> responses = ImmutableList.copyOf(results.getResponses().values());
-    String jsonResponse =
-        robotSerializer.serialize(responses, GsonFactory.JSON_RPC_RESPONSE_LIST_TYPE, version);
-    LOG.info("Returning the following Json: " + jsonResponse);
-
-    // Write the response back through the HttpServlet
-    try {
-      resp.setContentType(JSON_CONTENT_TYPE);
-      PrintWriter writer = resp.getWriter();
-      writer.append(jsonResponse);
-      writer.flush();
-      resp.setStatus(HttpServletResponse.SC_OK);
-    } catch (IOException e) {
-      LOG.severe("IOException during writing of a response", e);
-      throw e;
-    }
+    processOpsRequest(req, resp, message, accessor, participant);
   }
 }
