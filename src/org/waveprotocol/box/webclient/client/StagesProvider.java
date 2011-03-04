3b551b0139959bc26ed0c86840007a81621dcce7
Yuri Z <vega113@gmail.com>
2011-03-04 14:02:01 +1100

    Filters waves without conversational wavelets.

--- src/org/waveprotocol/box/webclient/client/StagesProvider.java
+++ src/org/waveprotocol/box/webclient/client/StagesProvider.java
@@ -34,9 +34,11 @@ import org.waveprotocol.wave.client.common.util.LogicalPanel;
 import org.waveprotocol.wave.client.wavepanel.view.BlipView;
 import org.waveprotocol.wave.client.wavepanel.view.dom.ModelAsViewProvider;
 import org.waveprotocol.wave.client.wavepanel.view.dom.full.BlipQueueRenderer;
+import org.waveprotocol.wave.model.conversation.Conversation;
 import org.waveprotocol.wave.model.conversation.ConversationBlip;
 import org.waveprotocol.wave.model.conversation.ConversationView;
 import org.waveprotocol.wave.model.id.IdGenerator;
+import org.waveprotocol.wave.model.id.ModernIdSerialiser;
 import org.waveprotocol.wave.model.waveref.WaveRef;
 
 /**
@@ -170,12 +172,23 @@ public class StagesProvider extends Stages {
       BlipQueueRenderer blipQueue = two.getBlipQueue();
       ConversationView wave = two.getConversations();
       blipQueue.flush();
-      // Find selected blip.
-      ConversationBlip blip = wave.getRoot().getBlip(documentId);
-      if (blip != null) {
-        BlipView blipUi = views.getBlipView(blip);
-        if (blipUi != null) {
-          three.getEditActions().focus(blipUi);
+      // Find conversation
+      Conversation conversation;
+      if (waveRef.hasWaveletId()) {
+        String id =  waveRef.getWaveletId().serialise();
+        conversation = wave.getConversation(id);
+      } else {
+        // Unspecified wavelet means root.
+        conversation = wave.getRoot();
+      }
+      if (conversation != null) {
+        // Find selected blip.
+        ConversationBlip blip = wave.getRoot().getBlip(documentId);
+        if (blip != null) {
+          BlipView blipUi = views.getBlipView(blip);
+          if (blipUi != null) {
+            three.getEditActions().focus(blipUi);
+          }
         }
       }
     }
