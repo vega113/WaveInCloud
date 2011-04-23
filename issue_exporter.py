4823300cde4244a9452191a9b3177759c4c99730
Michael MacFadden <michael.macfadden@gmail.com>
2011-04-23 10:06:16 -0700

    - The id was shortened to simply the issue Id number from the url. - The columns were re-ordered and renamed to line up with how Jira typically orders them. - The comments from the google code issue were moved from the description to a single comment column.   This way the comments will appear as a single comment in the Jira issue. - The handling of the labels was enhanced to specifically look for the priority and type labels using string parsing. - The column values for Priority and Type are now mapped from the Google Code statuses to the corresponding Jira values. - Google Code only has the concept of status, where as Jira has both a status (open/closed) and a resolution (fixed,   duplicate, etc).  The single Google Code status is now mapped to two columns in the export representing status and   resolution. - General code cleanup and commenting.

--- issue_exporter.py
+++ issue_exporter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright 2011 James Purser
-# Copyright 2011 Christian Ohler
+# Copyright 2011 James Purser, Christian Ohler, Michael MacFadden
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,24 +23,76 @@ In order to run the script you will need the following library:
 GData: http://code.google.com/p/gdata-python-client/"""
 
 import gdata.projecthosting.client
+import os
+
+""" Configuration Values """
 
 project_name = "wave-protocol"
+export_file_name = "exported_issues.csv"
 
-client = gdata.projecthosting.client.ProjectHostingClient()
+"""Mappings between Google Code Values and Jira Values"""
+types = {}
+types["Defect"] = "Bug"
+types["Enhancement"] = "Improvemnet"
+types["Patch"] = "Bug"
+types["Review"] = "Bug"
+types["Task"] = "Task"
 
-f = open("issues.csv", "w")
-# First row with column titles
-f.write("Title,Id,Type,Priority,Status,Description\n")
+priorities = {}
+priorities["Critical"] = "Critical"
+priorities["High"] = "Major"
+priorities["Medium"] = "Minor"
+priorities["Low"] = "Trivial"
+
+statuses = {}
+statuses["New"] = "Open"
+statuses["Accepted"] = "Open"
+statuses["Started"] = "In Progress"
+statuses["Fixed"] = "Closed"
+statuses["Invalid"] = "Closed"
+statuses["Duplicate"] = "Closed"
+statuses["WontFix"] = "Closed"
+
+resolutions = {}
+resolutions["New"] = ""
+resolutions["Accepted"] = ""
+resolutions["Started"] = ""
+resolutions["Fixed"] = "Fixed"
+resolutions["Invalid"] = "Invalid"
+resolutions["Duplicate"] = "Duplicate"
+resolutions["WontFix"] = "Won't Fix"
+
+""" Helper Functions """
 
 def get_comments(issue_id):
     comments = []
     while True:
         comments_query = gdata.projecthosting.client.Query(start_index=len(comments) + 1)
-        comments_feed = client.get_comments(project_name, issue_id, query=comments_query)
+        comments_feed = hosting_client.get_comments(project_name, issue_id, query=comments_query)
         if not comments_feed.entry:
             break
         comments.extend(comments_feed.entry)
-    return comments
+        
+    comments_text = ""
+    
+    if len(comments) > 0:
+		for comment in comments:
+			if len(comments_text) > 0:
+			    comments_text += "---\n"
+			comments_text += "%s:\n%s\n\n" % (comment.title.text, comment.content.text)
+			updates = comment.updates
+			if updates:
+				if updates.summary:
+					comments_text += "Summary: %s\n\n" % updates.summary.text
+				if updates.status:
+					comments_text += "Status: %s\n\n" % updates.status.text
+				if updates.ownerUpdate:
+					comments_text += "OwnerUpdate: %s\n\n" % updates.ownerUpdate.text
+				for label in updates.label:
+					comments_text += "Label: %s\n\n" % label.text
+				for ccUpdate in updates.ccUpdate:
+					comments_text += "CcUpdate: %s\n\n" % ccUpdate.text	
+    return comments_text
 
 def escape(s):
     return '"' + s.encode('utf-8').replace('"', '""') + '"'
@@ -52,7 +103,7 @@ def get_id(issue):
 def get_link(issue):
     return "http://code.google.com/p/%s/issues/detail?id=%s" % (project_name, get_id(issue))
 
-def get_description(issue, comments):
+def get_description(issue):
     # TODO: The content seems to be HTML, I'm not sure if that's what JIRA expects.
     description = issue.content.text
     description += "\n\n---\nIssue imported from %s\n\n" % issue_link
@@ -68,62 +119,72 @@ def get_description(issue, comments):
         description += "State: %s\n" % issue.state.text
     if issue.status:
         description += "Status: %s\n" % issue.status.text
-    for comment in comments:
-        description += "---\n%s:\n%s\n\n" % (comment.title.text, comment.content.text)
-        updates = comment.updates
-        if updates:
-            if updates.summary:
-                description += "Summary: %s\n\n" % updates.summary.text
-            if updates.status:
-                description += "Status: %s\n\n" % updates.status.text
-            if updates.ownerUpdate:
-                description += "OwnerUpdate: %s\n\n" % updates.ownerUpdate.text
-            for label in updates.label:
-                description += "Label: %s\n\n" % label.text
-            for ccUpdate in updates.ccUpdate:
-                description += "CcUpdate: %s\n\n" % ccUpdate.text
     return description
+    
+def convert_value(list,value):
+	if len(value) > 0:
+	    return list[value]
+	else:
+		return ""
 
+""" Export Process """
+
+hosting_client = gdata.projecthosting.client.ProjectHostingClient()
+export_file = open(export_file_name, "w")
+
+# First row with column titles
+export_file.write("Id,Summary,Issue Type,Priority,Status,Resolution,Description,Comment Body\n")
+
+print "Exporting issues to: " + os.path.abspath(export_file_name)
 
 issues_total = 0
 while True:
     issues_query = gdata.projecthosting.client.Query(start_index=issues_total + 1)
-    issues_feed = client.get_issues(project_name, query=issues_query)
+    issues_feed = hosting_client.get_issues(project_name, query=issues_query)
     if not issues_feed.entry:
         break
     for issue in issues_feed.entry:
         issue_id = get_id(issue)
         issue_link = get_link(issue)
-        print "issue %s: %s" % (issue_id, issue_link)
-        comments = get_comments(issue_id)
-        print "issue %s: %s comments" % (issue_id, len(comments))
+        print "Exporting Issue %s: %s" % (issue_id, issue_link)
+        
         # Append one row per issue
-        # Title
-        row = escape(issue.title.text) + ","
+
         # Id, e.g. http://code.google.com/feeds/issues/p/wave-protocol/issues/full/1
-        row += escape(issue.id.text) + ","
+        row = escape(issue_id) + ","
+        
+        # Title
+        row += escape(issue.title.text) + ","
+        
+        # Fetch the type and priority from the labels
+    	# There are any number of labels, but we are only interested in two.  Namely 
+    	# Type-* and Priority-*
         type = "";
         priority = ""; 
-        # There are any number of labels, but we are only interested in two.  Namely Type-* and Priority-*
         for label in issue.label:
             if label.text.startswith("Type-"):
                 type = label.text[5:]
             elif label.text.startswith("Priority-"):
                 priority = label.text[9:]
-        row += escape(type) + ","
-        row += escape(priority) + ","
-        # Status, e.g. Fixed
-        row += escape(issue.status.text) + ","
-        # TODO: figure out how to pull the relevant attributes out of Owner, Author
-        #row += issue.owner + ","     # type gdata.projecthosting.data.Owner
-        #row += issue.author[0] + "," # type atom.data.Author
-
-        description = get_description(issue, comments)
-        print description
-
-        row += escape(description)
-        f.write(row + "\n")
+        row += escape(convert_value(types,type)) + ","
+        row += escape(convert_value(priorities,priority)) + ","
+        
+        # Google Code only has the concept of status, while Jira has status and resolution.
+        # Status, e.g. Open or Closed
+        row += escape(convert_value(statuses,issue.status.text)) + ","
+        # Status, e.g. Fixed, Invalid, etc
+        row += escape(convert_value(resolutions,issue.status.text)) + ","
+        
+		# Fetch the description text
+        description = get_description(issue)
+        row += escape(description) + ","
+        
+        # Fetch all the comments and put them in a single column
+        comments = get_comments(issue_id)
+        row += escape(comments)
+        
+        export_file.write(row + "\n")
     issues_total += len(issues_feed.entry)
 
-f.close()
-print "%s issues total" % issues_total
+export_file.close()
+print "\n%s issues exported" % issues_total
