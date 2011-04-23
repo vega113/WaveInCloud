3323269eded1a53e3390cf83cac8d4ff69a45419
ohler@google.com <ohler@google.com>
2011-04-23 02:51:39 +0000

    issue_exporter: Get all issues, not just 25, and embed the comments in the description rather than dropping them.
    Also assert that there are exactly 2 labels rather than producing garbage output if there aren't.  This assertion currently fails; someone who understands the desired output format will need to find a solution to this.

--- issue_exporter.py
+++ issue_exporter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #
 # Copyright 2011 James Purser
+# Copyright 2011 Christian Ohler
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,34 +25,100 @@ GData: http://code.google.com/p/gdata-python-client/"""
 
 import gdata.projecthosting.client
 
+project_name = "wave-protocol"
+
 client = gdata.projecthosting.client.ProjectHostingClient()
-# Grab the first 25 issues
-# TODO: figure out why we only get 25 issues and how to get the rest
-feed = client.get_issues("wave-protocol")
 
 f = open("issues.csv", "w")
-
 # First row with column titles
 f.write("Title,Id,Type,Priority,Status,Description\n")
 
-# Append one row per issue
-for issue in feed.entry:
-    # Title
-    row = "\"" + issue.title.text + "\","
-    # Id, e.g. http://code.google.com/feeds/issues/p/wave-protocol/issues/full/1
-    row += issue.id.text + ","
-    # There are exactly two labels, Type (e.g. Type-Defect) and
-    # Priority (e.g. Priority-Medium)
-    for labels in issue.label:
-        row += labels.text + ","
-    # Status, e.g. Fixed
-    row += issue.status.text + ","
-    # TODO: figure out how to pull the relevant attributes out of Owner, Author
-    #row += issue.owner + ","     # type gdata.projecthosting.data.Owner
-    #row += issue.author[0] + "," # type atom.data.Author
-    # Description
-    row += "\"" + issue.content.text.encode('utf-8') + "\","
-    # TODO: figure out how to retrieve the replies (they are not in issue)
-    f.write(row + "\n")
+def get_comments(issue_id):
+    comments = []
+    while True:
+        comments_query = gdata.projecthosting.client.Query(start_index=len(comments) + 1)
+        comments_feed = client.get_comments(project_name, issue_id, query=comments_query)
+        if not comments_feed.entry:
+            break
+        comments.extend(comments_feed.entry)
+    return comments
+
+def escape(s):
+    return '"' + s.encode('utf-8').replace('"', '""') + '"'
+
+def get_id(issue):
+    return issue.get_id().split('/')[-1]
+
+def get_link(issue):
+    return "http://code.google.com/p/%s/issues/detail?id=%s" % (project_name, get_id(issue))
+
+def get_description(issue, comments):
+    # TODO: The content seems to be HTML, I'm not sure if that's what JIRA expects.
+    description = issue.content.text
+    description += "\n\n---\nIssue imported from %s\n\n" % issue_link
+    if issue.owner:
+        description += "Owner: %s\n" % issue.owner.username.text
+    for cc in issue.cc:
+        description += "Cc: %s\n" % cc.username.text
+    for label in issue.label:
+        description += "Label: %s\n" % label.text
+    if issue.stars:
+        description += "Stars: %s\n" % issue.stars.text
+    if issue.state:
+        description += "State: %s\n" % issue.state.text
+    if issue.status:
+        description += "Status: %s\n" % issue.status.text
+    for comment in comments:
+        description += "---\n%s:\n%s\n\n" % (comment.title.text, comment.content.text)
+        updates = comment.updates
+        if updates:
+            if updates.summary:
+                description += "Summary: %s\n\n" % updates.summary.text
+            if updates.status:
+                description += "Status: %s\n\n" % updates.status.text
+            if updates.ownerUpdate:
+                description += "OwnerUpdate: %s\n\n" % updates.ownerUpdate.text
+            for label in updates.label:
+                description += "Label: %s\n\n" % label.text
+            for ccUpdate in updates.ccUpdate:
+                description += "CcUpdate: %s\n\n" % ccUpdate.text
+    return description
+
+
+issues_total = 0
+while True:
+    issues_query = gdata.projecthosting.client.Query(start_index=issues_total + 1)
+    issues_feed = client.get_issues(project_name, query=issues_query)
+    if not issues_feed.entry:
+        break
+    for issue in issues_feed.entry:
+        issue_id = get_id(issue)
+        issue_link = get_link(issue)
+        print "issue %s: %s" % (issue_id, issue_link)
+        comments = get_comments(issue_id)
+        print "issue %s: %s comments" % (issue_id, len(comments))
+        # Append one row per issue
+        # Title
+        row = escape(issue.title.text) + ","
+        # Id, e.g. http://code.google.com/feeds/issues/p/wave-protocol/issues/full/1
+        row += escape(issue.id.text) + ","
+        # There are exactly two labels, Type (e.g. Type-Defect) and
+        # Priority (e.g. Priority-Medium)
+        assert len(issue.label) == 2  # NOTE(ohler): this assertion fails
+        for labels in issue.label:
+            row += escape(labels.text) + ","
+        # Status, e.g. Fixed
+        row += escape(issue.status.text) + ","
+        # TODO: figure out how to pull the relevant attributes out of Owner, Author
+        #row += issue.owner + ","     # type gdata.projecthosting.data.Owner
+        #row += issue.author[0] + "," # type atom.data.Author
+
+        description = get_description(issue, comments)
+        print description
+
+        row += escape(description)
+        f.write(row + "\n")
+    issues_total += len(issues_feed.entry)
 
 f.close()
+print "%s issues total" % issues_total
