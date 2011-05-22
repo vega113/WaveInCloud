c990242ebc80ab11aabbcdae7ed00ce580300c65
sorenlassen@gmail.com <sorenlassen@gmail.com>
2011-05-22 10:45:38 -0700

    Exclude null views. Fixes breakage from rev b68b6c782e57.

--- src/org/waveprotocol/box/server/waveserver/WaveMap.java
+++ src/org/waveprotocol/box/server/waveserver/WaveMap.java
@@ -769,7 +769,9 @@ public class WaveMap implements SearchProvider {
           LOG.warning("Failed to access wavelet " + c.getWaveletName(), e);
         }
       }
-      results.put(waveId, view);
+      if (view != null) {
+	  results.put(waveId, view);
+      }
       // TODO (Yuri Z.) Investigate if it worth to keep the waves views sorted
       // by LMT so we can stop looping after we have (startAt + numResults)
       // results.
