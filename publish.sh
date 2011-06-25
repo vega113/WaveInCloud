b4357221038500ae27eb758efa06de655110a4c8
Yuri Zelikov <vega113@gmail.com>
2011-06-25 15:29:08 +0300

    Adds a script to publish the WIAb server version to Amazon S3 bucket.

---
+++ publish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ant clean
+ant compile-gwt
+ant dist-server
+rm ./dist/publish/publish.zip
+mkdir ./dist/publish
+zip -r ./dist/publish/publish.zip -xi ./dist/waveinabox-server-0.3.jar ./war/* ./*.properties
+synchronize.sh UP waveincloud/publish/dist/$(date +%b-%d) ./dist/publish/publish.zip
