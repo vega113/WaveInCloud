7c5edd39d418b4ce95dd6287fc55c2c3ef51dfb3
Yuri Zelikov <vega113@gmail.com>
2011-06-26 20:03:30 +0300

    Modifies the publish script to make the uploaded file public on S3.

--- publish.sh
+++ publish.sh
@@ -7,3 +7,4 @@ rm ./dist/publish/publish.zip
 mkdir ./dist/publish
 zip -r ./dist/publish/publish.zip -xi ./dist/waveinabox-server-0.3.jar ./war/* ./*.properties
 synchronize.sh UP waveincloud/publish/dist/$(date +%b-%d) ./dist/publish/publish.zip
+s3cmd setacl --acl-public s3://waveincloud/publish/dist/$(date +%b-%d)/publish.zip
