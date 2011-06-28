c704ca1d0b8b0b564c248972446876504e10d421
Yuri Zelikov <vega113@gmail.com>
2011-06-28 14:48:39 +0300

    Fixes publish script to work with universal time.

--- publish.sh
+++ publish.sh
@@ -6,5 +6,5 @@ ant dist-server
 rm ./dist/publish/publish.zip
 mkdir ./dist/publish
 zip -r ./dist/publish/publish.zip -xi ./dist/waveinabox-server-0.3.jar ./war/* ./*.properties
-synchronize.sh UP waveincloud/publish/dist/$(date +%b-%d) ./dist/publish/publish.zip
-s3cmd setacl --acl-public s3://waveincloud/publish/dist/$(date +%b-%d)/publish.zip
+synchronize.sh UP waveincloud/publish/dist/$(date --universal  +%b-%d) ./dist/publish/publish.zip
+s3cmd setacl --acl-public s3://waveincloud/publish/dist/$(date --universal  +%b-%d)/publish.zip
