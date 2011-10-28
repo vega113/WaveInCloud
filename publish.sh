47372a3660455fee68e8f0ea97624267ff60aa3a
Yuri Zelikov <vega113@gmail.com>
2011-10-28 20:02:14 +0200

    Fixes scripts.

--- publish.sh
+++ publish.sh
@@ -7,4 +7,5 @@ rm ./dist/publish/publish.zip
 mkdir ./dist/publish
 zip -r ./dist/publish/publish.zip -xi ./dist/waveinabox-server-0.3.jar ./war/* ./*.properties ./*.sh
 synchronize.sh UP waveincloud/publish/dist/$(date --universal  +%b-%d) ./dist/publish/publish.zip
-s3cmd setacl --acl-public s3://waveincloud/publish/dist/$(date --universal  +%b-%d)/publish.zip
+# Make sure to set the synchronize property to PUBLIC-READ. If you don't want to, try to use s3cmd to set ACL.
+#s3cmd setacl --acl-public s3://waveincloud/publish/dist/$(date --universal  +%b-%d)/publish.zip
