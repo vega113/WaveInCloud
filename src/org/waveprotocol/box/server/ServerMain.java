61e4c52419bb45b7c18564eed2c1e79b50c39500
Yuri Zelikov <yurize@apache.org>
2012-04-07 07:41:25 +0000

    Implements wave-319: Improve the 'Add gadget' popup. By wavegrove@gmail.com https://reviews.apache.org/r/3960
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wave/trunk@1310687 13f79535-47bb-0310-9956-ffa450edef68
    
    Conflicts:
    
    	jsongadgets.json

--- src/org/waveprotocol/box/server/ServerMain.java
+++ src/org/waveprotocol/box/server/ServerMain.java
@@ -53,6 +53,7 @@ import org.waveprotocol.box.server.rpc.AttachmentServlet;
 import org.waveprotocol.box.server.rpc.AuthenticationServlet;
 import org.waveprotocol.box.server.rpc.FetchProfilesServlet;
 import org.waveprotocol.box.server.rpc.FetchServlet;
+import org.waveprotocol.box.server.rpc.GadgetProviderServlet;
 import org.waveprotocol.box.server.rpc.SearchServlet;
 import org.waveprotocol.box.server.rpc.ServerRpcProvider;
 import org.waveprotocol.box.server.rpc.SignOutServlet;
@@ -214,6 +215,7 @@ public class ServerMain {
   }
 
   private static void initializeServlets(Injector injector, ServerRpcProvider server) {
+    server.addServlet("/gadget/gadgetlist", GadgetProviderServlet.class);
     server.addServlet("/attachment/*", AttachmentServlet.class);
 
     server.addServlet(SessionManager.SIGN_IN_URL, AuthenticationServlet.class);
